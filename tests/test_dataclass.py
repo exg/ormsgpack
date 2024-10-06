019e19e07a01c7c8d59899c49ed753e7e92cc602
Emanuele Giaquinta <emanuele.giaquinta@gmail.com>
2024-10-06 17:27:44 +0300

    Refactor dataclass tests
    Signed-off-by: Emanuele Giaquinta <emanuele.giaquinta@gmail.com>

--- tests/test_dataclass.py
+++ tests/test_dataclass.py
@@ -1,7 +1,5 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-import abc
 from dataclasses import InitVar, asdict, dataclass, field
-from enum import Enum
 from typing import ClassVar, Optional
 
 import msgpack
@@ -10,198 +8,244 @@ import pytest
 import ormsgpack
 
 
-class AnEnum(Enum):
-    ONE = 1
-    TWO = 2
-
-
-@dataclass
-class EmptyDataclass:
-    pass
-
-
-@dataclass
-class EmptyDataclassSlots:
-    __slots__ = ()
-
-
-@dataclass
-class Dataclass1:
-    name: str
-    number: int
-    sub: Optional["Dataclass1"]
-
-
-@dataclass
-class Dataclass2:
-    name: Optional[str] = field(default="?")
-
-
-@dataclass
-class Dataclass4:
-    a: str = field()
-    b: int = field(metadata={"unrelated": False})
-    c: float = 1.1
-
-
-@dataclass
-class Datasubclass(Dataclass1):
-    additional: bool
-
-
-@dataclass
-class Slotsdataclass:
-    __slots__ = ("a", "b", "_c", "d")
-    a: str
-    b: int
-    _c: str
-    d: InitVar[str]
-    cls_var: ClassVar[str] = "cls"
-
-
-@dataclass
-class InitDataclass:
-    a: InitVar[str]
-    b: InitVar[str]
-    cls_var: ClassVar[str] = "cls"
-    ab: str = ""
-
-    def __post_init__(self, a: str, b: str):
-        self._other = 1
-        self.ab = f"{a} {b}"
-
-
-class AbstractBase(abc.ABC):
-    @abc.abstractmethod
-    def key(self):
-        raise NotImplementedError
-
-
-@dataclass(frozen=True)
-class ConcreteAbc(AbstractBase):
-    __slots__ = ("attr",)
-
-    attr: float
-
-    def key(self):
-        return "dkjf"
-
-
-def test_dataclass():
+def test_dataclass() -> None:
     """
     packb() dataclass
     """
-    obj = Dataclass1("a", 1, None)
+
+    @dataclass
+    class Dataclass:
+        a: str
+        b: int
+        c: InitVar[str]
+        d: ClassVar[str] = "cls"
+
+    obj = Dataclass("a", 1, "")
     assert ormsgpack.packb(obj) == msgpack.packb(
-        {"name": "a", "number": 1, "sub": None}
+        {
+            "a": "a",
+            "b": 1,
+        }
     )
 
 
-def test_dataclass_recursive():
+def test_dataclass_with_slots() -> None:
+    """
+    packb() dataclass with slots
+    """
+
+    @dataclass
+    class Dataclass:
+        a: str
+        b: int
+        c: InitVar[str]
+        d: ClassVar[str] = "cls"
+
+        __slots__ = (
+            "a",
+            "b",
+            "c",
+        )
+
+    obj = Dataclass("a", 1, "")
+    assert ormsgpack.packb(obj) == msgpack.packb(
+        {
+            "a": "a",
+            "b": 1,
+        }
+    )
+
+
+def test_dataclass_empty() -> None:
+    """
+    packb() dataclass with no attributes
+    """
+
+    @dataclass
+    class Dataclass:
+        pass
+
+    assert ormsgpack.packb(Dataclass()) == msgpack.packb({})
+
+
+def test_dataclass_empty_with_slots() -> None:
+    """
+    packb() dataclass with no attributes and slots
+    """
+
+    @dataclass
+    class Dataclass:
+        __slots__ = ()
+
+    assert ormsgpack.packb(Dataclass()) == msgpack.packb({})
+
+
+def test_dataclass_with_private_field() -> None:
+    """
+    packb() dataclass with private field
+    """
+
+    @dataclass
+    class Dataclass:
+        a: str
+        b: int
+        _c: str
+
+    obj = Dataclass("a", 1, "")
+    assert ormsgpack.packb(obj) == msgpack.packb(
+        {
+            "a": "a",
+            "b": 1,
+        }
+    )
+
+
+def test_dataclass_with_private_field_and_slots() -> None:
+    """
+    packb() dataclass with private field and slots
+    """
+
+    @dataclass
+    class Dataclass:
+        a: str
+        b: int
+        _c: str
+
+        __slots__ = (
+            "a",
+            "b",
+            "_c",
+        )
+
+    obj = Dataclass("a", 1, "")
+    assert ormsgpack.packb(obj) == msgpack.packb(
+        {
+            "a": "a",
+            "b": 1,
+        }
+    )
+
+
+def test_dataclass_with_dict_and_slots() -> None:
+    class Base:
+        pass
+
+    @dataclass
+    class Dataclass(Base):
+        a: str
+        b: int
+
+        __slots__ = (
+            "a",
+            "b",
+        )
+
+    obj = Dataclass("a", 1)
+    assert hasattr(obj, "__dict__")
+    assert ormsgpack.packb(obj) == msgpack.packb(
+        {
+            "a": "a",
+            "b": 1,
+        }
+    )
+
+
+def test_dataclass_recursive() -> None:
     """
     packb() dataclass recursive
     """
-    obj = Dataclass1("a", 1, Dataclass1("b", 2, None))
+
+    @dataclass
+    class Dataclass:
+        a: str
+        b: int
+        c: Optional["Dataclass"]
+
+    obj = Dataclass("a", 1, Dataclass("b", 2, None))
     assert ormsgpack.packb(obj) == msgpack.packb(
-        {"name": "a", "number": 1, "sub": {"name": "b", "number": 2, "sub": None}}
+        {
+            "a": "a",
+            "b": 1,
+            "c": {
+                "a": "b",
+                "b": 2,
+                "c": None,
+            },
+        }
     )
 
 
-def test_dataclass_circular():
+def test_dataclass_circular() -> None:
     """
     packb() dataclass circular
     """
-    obj1 = Dataclass1("a", 1, None)
-    obj2 = Dataclass1("b", 2, obj1)
-    obj1.sub = obj2
+
+    @dataclass
+    class Dataclass:
+        a: str
+        b: int
+        c: Optional["Dataclass"]
+
+    obj1 = Dataclass("a", 1, None)
+    obj2 = Dataclass("b", 2, obj1)
+    obj1.c = obj2
     with pytest.raises(ormsgpack.MsgpackEncodeError):
         ormsgpack.packb(obj1)
 
 
-def test_dataclass_empty():
-    """
-    packb() no attributes
-    """
-    assert ormsgpack.packb(EmptyDataclass()) == msgpack.packb({})
-
-
-def test_dataclass_empty_slots():
-    """
-    packb() no attributes slots
-    """
-    assert ormsgpack.packb(EmptyDataclassSlots()) == msgpack.packb({})
-
-
-def test_dataclass_default_arg():
-    """
-    packb() dataclass default arg
-    """
-    obj = Dataclass2()
-    assert ormsgpack.packb(obj) == msgpack.packb({"name": "?"})
-
-
-def test_dataclass_metadata():
-    """
-    packb() dataclass metadata
-    """
-    obj = Dataclass4("a", 1, 2.1)
-    assert ormsgpack.packb(obj) == msgpack.packb({"a": "a", "b": 1, "c": 2.1})
-
-
-def test_dataclass_classvar():
-    """
-    packb() dataclass class variable
-    """
-    obj = Dataclass4("a", 1)
-    assert ormsgpack.packb(obj) == msgpack.packb({"a": "a", "b": 1, "c": 1.1})
-
-
-def test_dataclass_subclass():
+def test_dataclass_subclass() -> None:
     """
     packb() dataclass subclass
     """
-    obj = Datasubclass("a", 1, None, False)
+
+    @dataclass
+    class Dataclass:
+        a: str
+
+    @dataclass
+    class Datasubclass(Dataclass):
+        b: int
+
+    obj = Datasubclass("a", 1)
     assert ormsgpack.packb(obj) == msgpack.packb(
-        {"name": "a", "number": 1, "sub": None, "additional": False}
+        {
+            "a": "a",
+            "b": 1,
+        }
     )
 
 
-def test_dataclass_slots():
-    """
-    packb() dataclass with __slots__ does not include under attributes, InitVar, or ClassVar
-    """
-    obj = Slotsdataclass("a", 1, "c", "d")
-    assert "__dict__" not in dir(obj)
-    assert ormsgpack.packb(obj) == msgpack.packb({"a": "a", "b": 1})
-
-
-def test_dataclass_under():
-    """
-    packb() does not include under attributes, InitVar, or ClassVar
-    """
-    obj = InitDataclass("zxc", "vbn")
-    assert ormsgpack.packb(obj) == msgpack.packb({"ab": "zxc vbn"})
-
-
-def test_dataclass_passthrough_raise():
+def test_dataclass_passthrough() -> None:
     """
     packb() dataclass passes to default with OPT_PASSTHROUGH_DATACLASS
     """
-    obj = Dataclass1("a", 1, None)
+
+    @dataclass
+    class Dataclass:
+        a: str
+        b: int
+
+    obj = Dataclass("a", 1)
     with pytest.raises(ormsgpack.MsgpackEncodeError):
         ormsgpack.packb(obj, option=ormsgpack.OPT_PASSTHROUGH_DATACLASS)
 
 
-def test_dataclass_passthrough_default():
+def test_dataclass_passthrough_default() -> None:
     """
     packb() dataclass passes to default with OPT_PASSTHROUGH_DATACLASS
     """
-    obj = Dataclass1("a", 1, None)
+
+    @dataclass
+    class Dataclass:
+        a: str
+        b: int
+
+    obj = Dataclass("a", 1)
     assert ormsgpack.packb(
         obj, option=ormsgpack.OPT_PASSTHROUGH_DATACLASS, default=asdict
-    ) == msgpack.packb({"name": "a", "number": 1, "sub": None})
-
-
-def test_dataclass_abc():
-    obj = ConcreteAbc(1.0)
-    assert ormsgpack.packb(obj) == msgpack.packb({"attr": 1.0})
+    ) == msgpack.packb(
+        {
+            "a": "a",
+            "b": 1,
+        }
+    )
