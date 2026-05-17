28ffa98892f101f020eb47ee9f53ea79c6831d61
Emanuele Giaquinta <emanuele.giaquinta@gmail.com>
2026-05-17 08:26:56 +0300

    Use a guard for default hook recursion tracking
    Signed-off-by: Emanuele Giaquinta <emanuele.giaquinta@gmail.com>

--- src/serialize/default.rs
+++ src/serialize/default.rs
@@ -31,6 +31,19 @@ pub struct DefaultHook {
     recursion: Cell<u8>,
 }
 
+pub struct DefaultHookCall<'a> {
+    hook: &'a DefaultHook,
+    pub result: *mut pyo3::ffi::PyObject,
+}
+
+impl Drop for DefaultHookCall<'_> {
+    fn drop(&mut self) {
+        unsafe { pyo3::ffi::Py_DECREF(self.result) };
+        let recursion = self.hook.recursion.get();
+        self.hook.recursion.set(recursion - 1);
+    }
+}
+
 impl DefaultHook {
     pub fn new(default: Option<NonNull<pyo3::ffi::PyObject>>) -> Self {
         DefaultHook {
@@ -39,30 +52,25 @@ impl DefaultHook {
         }
     }
 
-    pub fn enter_call(
-        &self,
-        ptr: *mut pyo3::ffi::PyObject,
-    ) -> Result<*mut pyo3::ffi::PyObject, Error> {
+    pub fn call(&self, ptr: *mut pyo3::ffi::PyObject) -> Result<DefaultHookCall<'_>, Error> {
         match self.inner {
             Some(callable) => {
                 let recursion = self.recursion.get();
                 if unlikely(recursion == RECURSION_LIMIT) {
                     return Err(Error::RecursionLimitReached);
                 }
-                self.recursion.set(recursion + 1);
                 let default_obj = unsafe { pyobject_call_one_arg(callable.as_ptr(), ptr) };
                 if unlikely(default_obj.is_null()) {
                     Err(Error::InvalidType(ptr))
                 } else {
-                    Ok(default_obj)
+                    self.recursion.set(recursion + 1);
+                    Ok(DefaultHookCall {
+                        hook: self,
+                        result: default_obj,
+                    })
                 }
             }
             None => Err(Error::InvalidType(ptr)),
         }
-    }
-
-    pub fn leave_call(&self) {
-        let recursion = self.recursion.get();
-        self.recursion.set(recursion - 1);
     }
 }
